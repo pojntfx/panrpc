9d7f4681ac55a4a87f74fe7e1756a8f5fab5031f
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-12 20:05:35 +0100

    feat: Add add basic function generator

--- pkg/mockup/universal.go
+++ pkg/mockup/universal.go
@@ -1,8 +1,12 @@
 package mockup
 
 import (
+	"encoding/json"
+	"log"
 	"net"
 	"reflect"
+
+	"github.com/google/uuid"
 )
 
 type Registry[L any, R any] struct {
@@ -14,36 +18,55 @@ func NewRegistry[L any, R any](local L, remote R) *Registry[L, R] {
 	return &Registry[L, R]{local, remote}
 }
 
-func (r Registry[L, R]) link() {
-	reflect.
-		ValueOf(r.remote).
-		Elem().
-		FieldByName("Multiply").
-		Set(reflect.ValueOf(
-			func(multiplicant, multiplier float64) (product float64) {
-				return multiplicant * multiplicant
-			},
-		))
+func (r Registry[L, R]) Listen(lis net.Listener) error {
+	// for {
+	// 	conn, err := lis.Accept()
+	// 	if err != nil {
+	// 		log.Println("could not accept connection, continuing:", err)
 
-	reflect.
-		ValueOf(r.remote).
-		Elem().
-		FieldByName("Multiply").
-		Set(reflect.ValueOf(
-			func(multiplicant, multiplier float64) (product float64) {
-				return multiplicant * multiplicant
-			},
-		))
-}
+	// 		continue
+	// 	}
 
-func (r Registry[L, R]) Listen(local net.Listener) error {
-	r.link()
+	// 	log.Println(conn)
+
+	remote := reflect.
+		ValueOf(r.remote).
+		Elem()
+
+	for i := 0; i < remote.NumField(); i++ {
+		functionField := remote.Type().Field(i)
+		functionType := functionField.Type
+
+		fn := reflect.MakeFunc(functionType, func(args []reflect.Value) (results []reflect.Value) {
+			cmd := []any{true, uuid.NewString(), functionField.Name}
+
+			cmdArgs := []any{}
+			for _, arg := range args {
+				cmdArgs = append(cmdArgs, arg.Interface())
+			}
+			cmd = append(cmd, cmdArgs)
+
+			b, err := json.Marshal(cmd)
+			if err != nil {
+				panic(err)
+			}
+
+			// TODO: Write to peer connection, then wait for response and return it (see registry.go for the unmarshalling of the response and the JS implementation on how to handle responses - use the registry context.Context to abort if there is no response)
+
+			log.Println(string(b))
+
+			return []reflect.Value{
+				reflect.ValueOf(float64(25)),
+			}
+		})
+
+		remote.FieldByName(functionField.Name).Set(fn)
+	}
+	// }
 
 	return nil
 }
 
 func (r Registry[L, R]) Connect(conn net.Conn) error {
-	r.link()
-
 	return nil
 }
