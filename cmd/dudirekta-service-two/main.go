e00f13ad7dda03ca283b7a3f7713b65ed0b0f532
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-16 21:39:41 +0100

    feat: Add two-way RPC support

---
+++ cmd/dudirekta-service-two/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/pojntfx/dudirekta/pkg/mockup"
+)
+
+type local[P any] struct{}
+
+func (s local[P]) Divide(ctx context.Context, divident, divisor float64) (quotient float64, err error) {
+	if divisor == 0 {
+		return -1, errors.New("could not divide by zero")
+	}
+
+	return divident / divisor, nil
+}
+
+type remote struct {
+	Multiply      func(ctx context.Context, multiplicant, multiplier float64) (product float64)
+	PrintString   func(ctx context.Context, msg string)
+	ValidateEmail func(ctx context.Context, email string) error
+	ParseJSON     func(ctx context.Context, p []byte) (any, error)
+}
+
+func main() {
+	addr := flag.String("addr", "localhost:1337", "Listen or remote address")
+	listen := flag.Bool("listen", true, "Whether to allow connecting to peers by listening or dialing")
+
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	registry := mockup.NewRegistry(
+		&local[remote]{},
+		remote{},
+		ctx,
+	)
+
+	time.AfterFunc(time.Second*5, func() {
+		for peerID, peer := range registry.Peers() {
+			log.Println("Calling functions for peer with ID", peerID)
+
+			fmt.Println(peer.Multiply(ctx, 5, 2))
+
+			peer.PrintString(ctx, "Hello from remote!")
+
+			if err := peer.ValidateEmail(ctx, "anne@example.com"); err != nil {
+				log.Println("Got email validation error:", err)
+			}
+
+			if err := peer.ValidateEmail(ctx, "asdf"); err != nil {
+				log.Println("Got email validation error:", err)
+			}
+
+			res, err := peer.ParseJSON(ctx, []byte(`{"name": "Jane"}`))
+			if err != nil {
+				log.Println("Got JSON parser error:", err)
+			} else {
+				fmt.Println(res)
+			}
+
+			res, err = peer.ParseJSON(ctx, []byte(`{"n`))
+			if err != nil {
+				log.Println("Got JSON parser error:", err)
+			} else {
+				fmt.Println(res)
+			}
+		}
+	})
+
+	if *listen {
+		lis, err := net.Listen("tcp", *addr)
+		if err != nil {
+			panic(err)
+		}
+		defer lis.Close()
+
+		log.Printf("Listening on dudirekta+%v://%v", lis.Addr().Network(), lis.Addr().String())
+
+		if err := registry.Listen(lis); err != nil {
+			panic(err)
+		}
+	} else {
+		conn, err := net.Dial("tcp", *addr)
+		if err != nil {
+			panic(err)
+		}
+		defer conn.Close()
+
+		log.Printf("Connected to dudirekta+%v://%v", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
+
+		if err := registry.Connect(conn); err != nil {
+			panic(err)
+		}
+	}
+}
--- pkg/mockup/universal.go
+++ pkg/mockup/universal.go
@@ -54,253 +54,256 @@ func (r Registry[R]) Listen(lis net.Listener) error {
 				return
 			}
 
-			clients++
-
-			log.Printf("%v clients connected", clients)
-
-			defer func() {
-				clients--
-
-				if err := recover(); err != nil {
-					log.Printf("Client disconnected with error: %v", err)
-				}
+			go func() {
+				clients++
 
 				log.Printf("%v clients connected", clients)
-			}()
 
-			responseResolver := broadcast.NewRelay[response]()
-			go func() {
-				d := json.NewDecoder(conn)
+				defer func() {
+					clients--
 
-				for {
-					var res []json.RawMessage
-					if err := d.Decode(&res); err != nil {
-						panic(err)
+					if err := recover(); err != nil {
+						log.Printf("Client disconnected with error: %v", err)
 					}
 
-					var isCall bool
-					if err := json.Unmarshal(res[0], &isCall); err != nil {
-						panic(err)
-					}
+					log.Printf("%v clients connected", clients)
+				}()
 
-					var callID string
-					if err := json.Unmarshal(res[1], &callID); err != nil {
-						panic(err)
-					}
-
-					if isCall {
-						go func() {
-							var functionName string
-							if err := json.Unmarshal(res[2], &functionName); err != nil {
-								panic(err)
-							}
-
-							var functionArgs []json.RawMessage
-							if err := json.Unmarshal(res[3], &functionArgs); err != nil {
-								panic(err)
-							}
-
-							function := reflect.
-								ValueOf(r.local).
-								Elem().
-								MethodByName(functionName)
-
-							args := []reflect.Value{}
-							for i := 0; i < function.Type().NumIn(); i++ {
-								if i == 0 {
-									// Add the context to the function arguments
-									args = append(args, reflect.ValueOf(r.ctx))
-
-									continue
-								}
-
-								arg := reflect.New(function.Type().In(i))
-								if err := json.Unmarshal(functionArgs[i-1], arg.Interface()); err != nil {
-									panic(err)
-								}
-
-								args = append(args, arg.Elem())
-							}
-
-							go func() {
-								res := function.Call(args)
-								switch len(res) {
-								case 0:
-									b, err := json.Marshal([]interface{}{callID, nil, ""})
-									if err != nil {
-										panic(err)
-									}
-
-									if _, err := conn.Write(b); err != nil {
-										panic(err)
-									}
-								case 1:
-									if res[0].Type().Implements(errorType) {
-										b, err := json.Marshal([]interface{}{callID, nil, res[0].Interface().(error).Error()})
-										if err != nil {
-											panic(err)
-										}
-
-										if _, err := conn.Write(b); err != nil {
-											panic(err)
-										}
-									} else {
-										v, err := json.Marshal(res[0].Interface())
-										if err != nil {
-											panic(err)
-										}
-
-										b, err := json.Marshal([]interface{}{callID, json.RawMessage(string(v)), ""})
-										if err != nil {
-											panic(err)
-										}
-
-										if _, err := conn.Write(b); err != nil {
-											panic(err)
-										}
-									}
-								case 2:
-									v, err := json.Marshal(res[0].Interface())
-									if err != nil {
-										panic(err)
-									}
-
-									if res[1].Interface() == nil {
-										b, err := json.Marshal([]interface{}{callID, json.RawMessage(string(v)), ""})
-										if err != nil {
-											panic(err)
-										}
-
-										if _, err := conn.Write(b); err != nil {
-											panic(err)
-										}
-									} else {
-										b, err := json.Marshal([]interface{}{callID, json.RawMessage(string(v)), res[1].Interface().(error).Error()})
-										if err != nil {
-											panic(err)
-										}
-
-										if _, err := conn.Write(b); err != nil {
-											panic(err)
-										}
-									}
-								}
-							}()
-						}()
-
-						continue
-					}
-
-					var errMsg string
-					if err := json.Unmarshal(res[3], &errMsg); err != nil {
-						panic(err)
-					}
-
-					var err error
-					if strings.TrimSpace(errMsg) != "" {
-						err = errors.New(errMsg)
-					}
-
-					responseResolver.Broadcast(response{callID, res[2], err})
+				if err := r.Connect(conn); err != nil {
+					panic(err)
 				}
 			}()
-
-			remote := reflect.New(reflect.ValueOf(r.remote).Type()).Elem()
-
-			for i := 0; i < remote.NumField(); i++ {
-				functionField := remote.Type().Field(i)
-				functionType := functionField.Type
-
-				fn := reflect.MakeFunc(functionType, func(args []reflect.Value) (results []reflect.Value) {
-					callID := uuid.NewString()
-
-					cmd := []any{true, callID, functionField.Name}
-
-					cmdArgs := []any{}
-					for i, arg := range args {
-						if i == 0 {
-							// Don't sent the context over the wire
-
-							continue
-						}
-
-						cmdArgs = append(cmdArgs, arg.Interface())
-					}
-					cmd = append(cmd, cmdArgs)
-
-					b, err := json.Marshal(cmd)
-					if err != nil {
-						panic(err)
-					}
-
-					l := responseResolver.Listener(0)
-					defer l.Close()
-
-					res := make(chan response)
-					go func() {
-						for msg := range l.Ch() {
-							if msg.id == callID {
-								res <- msg
-
-								return
-							}
-						}
-					}()
-
-					if _, err := conn.Write(b); err != nil {
-						panic(err)
-					}
-
-					returnValues := []reflect.Value{}
-					select {
-					case rawReturnValue := <-res:
-						if functionType.NumOut() == 1 {
-							returnValue := reflect.New(functionType.Out(0))
-
-							if rawReturnValue.err != nil {
-								returnValue.Elem().Set(reflect.ValueOf(rawReturnValue.err))
-							} else if !functionType.Out(0).Implements(errorType) {
-								if err := json.Unmarshal(rawReturnValue.value, returnValue.Interface()); err != nil {
-									panic(err)
-								}
-							}
-
-							returnValues = append(returnValues, returnValue.Elem())
-						} else if functionType.NumOut() == 2 {
-							valueReturnValue := reflect.New(functionType.Out(0))
-							errReturnValue := reflect.New(functionType.Out(1))
-
-							if err := json.Unmarshal(rawReturnValue.value, valueReturnValue.Interface()); err != nil {
-								panic(err)
-							}
-
-							if rawReturnValue.err != nil {
-								errReturnValue.Elem().Set(reflect.ValueOf(rawReturnValue.err))
-							}
-
-							returnValues = append(returnValues, valueReturnValue.Elem(), errReturnValue.Elem())
-						}
-					case err := <-r.ctx.Done():
-						panic(err)
-					}
-
-					return returnValues
-				})
-
-				remote.FieldByName(functionField.Name).Set(fn)
-			}
-
-			remoteID := uuid.NewString()
-
-			r.remotesLock.Lock()
-			r.remotes[remoteID] = remote.Interface().(R)
-			r.remotesLock.Unlock()
 		}()
 	}
 }
 
 func (r Registry[R]) Connect(conn net.Conn) error {
-	return nil
+	responseResolver := broadcast.NewRelay[response]()
+
+	remote := reflect.New(reflect.ValueOf(r.remote).Type()).Elem()
+
+	for i := 0; i < remote.NumField(); i++ {
+		functionField := remote.Type().Field(i)
+		functionType := functionField.Type
+
+		fn := reflect.MakeFunc(functionType, func(args []reflect.Value) (results []reflect.Value) {
+			callID := uuid.NewString()
+
+			cmd := []any{true, callID, functionField.Name}
+
+			cmdArgs := []any{}
+			for i, arg := range args {
+				if i == 0 {
+					// Don't sent the context over the wire
+
+					continue
+				}
+
+				cmdArgs = append(cmdArgs, arg.Interface())
+			}
+			cmd = append(cmd, cmdArgs)
+
+			b, err := json.Marshal(cmd)
+			if err != nil {
+				panic(err)
+			}
+
+			l := responseResolver.Listener(0)
+			defer l.Close()
+
+			res := make(chan response)
+			go func() {
+				for msg := range l.Ch() {
+					if msg.id == callID {
+						res <- msg
+
+						return
+					}
+				}
+			}()
+
+			if _, err := conn.Write(b); err != nil {
+				panic(err)
+			}
+
+			returnValues := []reflect.Value{}
+			select {
+			case rawReturnValue := <-res:
+				if functionType.NumOut() == 1 {
+					returnValue := reflect.New(functionType.Out(0))
+
+					if rawReturnValue.err != nil {
+						returnValue.Elem().Set(reflect.ValueOf(rawReturnValue.err))
+					} else if !functionType.Out(0).Implements(errorType) {
+						if err := json.Unmarshal(rawReturnValue.value, returnValue.Interface()); err != nil {
+							panic(err)
+						}
+					}
+
+					returnValues = append(returnValues, returnValue.Elem())
+				} else if functionType.NumOut() == 2 {
+					valueReturnValue := reflect.New(functionType.Out(0))
+					errReturnValue := reflect.New(functionType.Out(1))
+
+					if err := json.Unmarshal(rawReturnValue.value, valueReturnValue.Interface()); err != nil {
+						panic(err)
+					}
+
+					if rawReturnValue.err != nil {
+						errReturnValue.Elem().Set(reflect.ValueOf(rawReturnValue.err))
+					}
+
+					returnValues = append(returnValues, valueReturnValue.Elem(), errReturnValue.Elem())
+				}
+			case err := <-r.ctx.Done():
+				panic(err)
+			}
+
+			return returnValues
+		})
+
+		remote.FieldByName(functionField.Name).Set(fn)
+	}
+
+	remoteID := uuid.NewString()
+
+	r.remotesLock.Lock()
+	r.remotes[remoteID] = remote.Interface().(R)
+	r.remotesLock.Unlock()
+
+	d := json.NewDecoder(conn)
+
+	for {
+		var res []json.RawMessage
+		if err := d.Decode(&res); err != nil {
+			panic(err)
+		}
+
+		var isCall bool
+		if err := json.Unmarshal(res[0], &isCall); err != nil {
+			panic(err)
+		}
+
+		var callID string
+		if err := json.Unmarshal(res[1], &callID); err != nil {
+			panic(err)
+		}
+
+		if isCall {
+			go func() {
+				var functionName string
+				if err := json.Unmarshal(res[2], &functionName); err != nil {
+					panic(err)
+				}
+
+				var functionArgs []json.RawMessage
+				if err := json.Unmarshal(res[3], &functionArgs); err != nil {
+					panic(err)
+				}
+
+				function := reflect.
+					ValueOf(r.local).
+					Elem().
+					MethodByName(functionName)
+
+				args := []reflect.Value{}
+				for i := 0; i < function.Type().NumIn(); i++ {
+					if i == 0 {
+						// Add the context to the function arguments
+						args = append(args, reflect.ValueOf(r.ctx))
+
+						continue
+					}
+
+					arg := reflect.New(function.Type().In(i))
+					if err := json.Unmarshal(functionArgs[i-1], arg.Interface()); err != nil {
+						panic(err)
+					}
+
+					args = append(args, arg.Elem())
+				}
+
+				go func() {
+					res := function.Call(args)
+					switch len(res) {
+					case 0:
+						b, err := json.Marshal([]interface{}{false, callID, nil, ""})
+						if err != nil {
+							panic(err)
+						}
+
+						if _, err := conn.Write(b); err != nil {
+							panic(err)
+						}
+					case 1:
+						if res[0].Type().Implements(errorType) && !res[0].IsNil() {
+							b, err := json.Marshal([]interface{}{false, callID, nil, res[0].Interface().(error).Error()})
+							if err != nil {
+								panic(err)
+							}
+
+							if _, err := conn.Write(b); err != nil {
+								panic(err)
+							}
+						} else {
+							v, err := json.Marshal(res[0].Interface())
+							if err != nil {
+								panic(err)
+							}
+
+							b, err := json.Marshal([]interface{}{false, callID, json.RawMessage(string(v)), ""})
+							if err != nil {
+								panic(err)
+							}
+
+							if _, err := conn.Write(b); err != nil {
+								panic(err)
+							}
+						}
+					case 2:
+						v, err := json.Marshal(res[0].Interface())
+						if err != nil {
+							panic(err)
+						}
+
+						if res[1].Interface() == nil {
+							b, err := json.Marshal([]interface{}{false, callID, json.RawMessage(string(v)), ""})
+							if err != nil {
+								panic(err)
+							}
+
+							if _, err := conn.Write(b); err != nil {
+								panic(err)
+							}
+						} else {
+							b, err := json.Marshal([]interface{}{false, callID, json.RawMessage(string(v)), res[1].Interface().(error).Error()})
+							if err != nil {
+								panic(err)
+							}
+
+							if _, err := conn.Write(b); err != nil {
+								panic(err)
+							}
+						}
+					}
+				}()
+			}()
+
+			continue
+		}
+
+		var errMsg string
+		if err := json.Unmarshal(res[3], &errMsg); err != nil {
+			panic(err)
+		}
+
+		var err error
+		if strings.TrimSpace(errMsg) != "" {
+			err = errors.New(errMsg)
+		}
+
+		responseResolver.Broadcast(response{callID, res[2], err})
+	}
 }
 
 func (r Registry[R]) Peers() map[string]R {
