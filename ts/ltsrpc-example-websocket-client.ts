e61e435e1612ac0f4bb1f5d12eec75fda6e84b7c
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-01-03 21:46:35 +0100

    feat: Use new `forRemotes` API in TypeScript websocket client example, use remote IDs in local function implementations

--- ts/ltsrpc-example-websocket-client.ts
+++ ts/ltsrpc-example-websocket-client.ts
@@ -3,7 +3,82 @@ import { env, exit, stdin, stdout } from "process";
 import { createInterface } from "readline/promises";
 import { ILocalContext, IRemoteContext, Registry } from "./index";
 
-const rl = createInterface({ input: stdin, output: stdout });
+let clients = 0;
+
+const registry = new Registry(
+  {
+    Println: async (ctx: ILocalContext, msg: string) => {
+      console.log("Printing message", msg, "for remote with ID", ctx.remoteID);
+
+      console.log(msg);
+    },
+  },
+  {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Increment: async (ctx: IRemoteContext, delta: number): Promise<number> => 0,
+  },
+  {
+    onClientConnect: () => {
+      clients++;
+
+      console.log(clients, "clients connected");
+    },
+    onClientDisconnect: () => {
+      clients--;
+
+      console.log(clients, "clients connected");
+    },
+  }
+);
+
+(async () => {
+  console.log(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
+
+- a: Increment remote counter by one
+- b: Decrement remote counter by one`);
+
+  const rl = createInterface({ input: stdin, output: stdout });
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const line =
+      // eslint-disable-next-line no-await-in-loop
+      await rl.question("");
+
+    // eslint-disable-next-line no-await-in-loop
+    await registry.forRemotes(async (remoteID, remote) => {
+      console.log("Calling functions for remote with ID", remoteID);
+
+      switch (line) {
+        case "a":
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            const res = await remote.Increment(undefined, 1);
+
+            console.log(res);
+          } catch (e) {
+            console.error(`Got error for Increment func: ${e}`);
+          }
+          break;
+
+        case "b":
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            const res = await remote.Increment(undefined, -1);
+
+            console.log(res);
+          } catch (e) {
+            console.error(`Got error for Increment func: ${e}`);
+          }
+
+          break;
+
+        default:
+          console.log(`Unknown letter ${line}, ignoring input`);
+      }
+    });
+  }
+})();
 
 const raddr = env.RADDR || "ws://127.0.0.1:1337";
 
@@ -20,19 +95,7 @@ await new Promise<void>((res, rej) => {
   socket.addEventListener("error", rej);
 });
 
-const registry = new Registry(
-  {
-    Println: async (ctx: ILocalContext, msg: string) => {
-      console.log(msg);
-    },
-  },
-  {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    Increment: async (ctx: IRemoteContext, delta: number): Promise<number> => 0,
-  }
-);
-
-const remote = registry.linkWebSocket(
+registry.linkWebSocket(
   socket,
 
   JSON.stringify,
@@ -43,42 +106,3 @@ const remote = registry.linkWebSocket(
 );
 
 console.log("Connected to", raddr);
-
-// eslint-disable-next-line no-constant-condition
-while (true) {
-  const line =
-    // eslint-disable-next-line no-await-in-loop
-    await rl.question(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
-
-- a: Increment remote counter by one
-- b: Decrement remote counter by one
-`);
-
-  switch (line) {
-    case "a":
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const res = await remote.Increment(undefined, 1);
-
-        console.log(res);
-      } catch (e) {
-        console.error(`Got error for Increment func: ${e}`);
-      }
-      break;
-
-    case "b":
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const res = await remote.Increment(undefined, -1);
-
-        console.log(res);
-      } catch (e) {
-        console.error(`Got error for Increment func: ${e}`);
-      }
-
-      break;
-
-    default:
-      console.log(`Unknown letter ${line}, ignoring input`);
-  }
-}
