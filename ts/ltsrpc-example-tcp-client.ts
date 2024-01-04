0c21a65d125315886d285789f27181e9f88e7dae
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-01-04 23:33:17 +0100

    feat: Port `listen()`/`dial` support to TCP client and servers respectively

--- ts/ltsrpc-example-tcp-client.ts
+++ ts/ltsrpc-example-tcp-client.ts
@@ -1,95 +1,149 @@
 /* eslint-disable no-console */
-import { Socket } from "net";
 import { env, exit, stdin, stdout } from "process";
 import { createInterface } from "readline/promises";
 import { parse } from "url";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Socket, createServer } from "net";
 import { ILocalContext, IRemoteContext, Registry } from "./index";
 
-const rl = createInterface({ input: stdin, output: stdout });
-
-const raddr = env.RADDR || "tcp://127.0.0.1:1337";
-
-const socket = new Socket();
-
-socket.on("error", (e) => {
-  console.error("Disconnected with error:", e.cause);
-
-  exit(1);
-});
-
-await new Promise<void>((res, rej) => {
-  const u = parse(raddr);
-
-  socket.connect(
-    {
-      host: u.hostname as string,
-      port: parseInt(u.port as string, 10),
-    },
-    res
-  );
-  socket.on("error", rej);
-});
+let clients = 0;
 
 const registry = new Registry(
   {
     Println: async (ctx: ILocalContext, msg: string) => {
+      console.log("Printing message", msg, "for remote with ID", ctx.remoteID);
+
       console.log(msg);
     },
   },
   {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     Increment: async (ctx: IRemoteContext, delta: number): Promise<number> => 0,
+  },
+  {
+    onClientConnect: () => {
+      clients++;
+
+      console.log(clients, "clients connected");
+    },
+    onClientDisconnect: () => {
+      clients--;
+
+      console.log(clients, "clients connected");
+    },
   }
 );
 
-const remote = registry.linkTCPSocket(
-  socket,
-
-  JSON.stringify,
-  JSON.parse,
-
-  (v) => v,
-  (v) => v
-);
-socket.on("close", () => exit(0));
-
-console.log("Connected to", raddr);
-
-// eslint-disable-next-line no-constant-condition
-while (true) {
-  const line =
-    // eslint-disable-next-line no-await-in-loop
-    await rl.question(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
+(async () => {
+  console.log(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
 
 - a: Increment remote counter by one
-- b: Decrement remote counter by one
-`);
+- b: Decrement remote counter by one`);
 
-  switch (line) {
-    case "a":
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const res = await remote.Increment(undefined, 1);
+  const rl = createInterface({ input: stdin, output: stdout });
 
-        console.log(res);
-      } catch (e) {
-        console.error(`Got error for Increment func: ${e}`);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const line =
+      // eslint-disable-next-line no-await-in-loop
+      await rl.question("");
+
+    // eslint-disable-next-line no-await-in-loop
+    await registry.forRemotes(async (remoteID, remote) => {
+      console.log("Calling functions for remote with ID", remoteID);
+
+      switch (line) {
+        case "a":
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            const res = await remote.Increment(undefined, 1);
+
+            console.log(res);
+          } catch (e) {
+            console.error(`Got error for Increment func: ${e}`);
+          }
+          break;
+
+        case "b":
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            const res = await remote.Increment(undefined, -1);
+
+            console.log(res);
+          } catch (e) {
+            console.error(`Got error for Increment func: ${e}`);
+          }
+
+          break;
+
+        default:
+          console.log(`Unknown letter ${line}, ignoring input`);
       }
-      break;
-
-    case "b":
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const res = await remote.Increment(undefined, -1);
-
-        console.log(res);
-      } catch (e) {
-        console.error(`Got error for Increment func: ${e}`);
-      }
-
-      break;
-
-    default:
-      console.log(`Unknown letter ${line}, ignoring input`);
+    });
   }
+})();
+
+const addr = env.ADDR || "127.0.0.1:1337";
+const listen = env.LISTEN === "true";
+
+if (listen) {
+  const u = parse(`tcp://${addr}`);
+
+  const server = createServer(async (socket) => {
+    socket.on("error", (e) => {
+      console.error("Client disconnected with error:", e);
+    });
+
+    registry.linkTCPSocket(
+      socket,
+
+      JSON.stringify,
+      JSON.parse,
+
+      (v) => v,
+      (v) => v
+    );
+  });
+
+  server.listen(
+    {
+      host: u.hostname as string,
+      port: parseInt(u.port as string, 10),
+    },
+    () => console.log("Listening on", addr)
+  );
+} else {
+  const u = parse(`tcp://${addr}`);
+
+  const socket = new Socket();
+
+  socket.on("error", (e) => {
+    console.error("Disconnected with error:", e.cause);
+
+    exit(1);
+  });
+  socket.on("close", () => exit(0));
+
+  await new Promise<void>((res, rej) => {
+    socket.connect(
+      {
+        host: u.hostname as string,
+        port: parseInt(u.port as string, 10),
+      },
+      res
+    );
+    socket.on("error", rej);
+  });
+
+  registry.linkTCPSocket(
+    socket,
+
+    JSON.stringify,
+    JSON.parse,
+
+    (v) => v,
+    (v) => v
+  );
+
+  console.log("Connected to", addr);
 }
--- ts/ltsrpc-example-tcp-server.ts
+++ ts/ltsrpc-example-tcp-server.ts
@@ -1,13 +1,10 @@
 /* eslint-disable no-console */
-import { createServer } from "net";
-import { env, stdin, stdout } from "process";
+import { env, exit, stdin, stdout } from "process";
 import { createInterface } from "readline/promises";
 import { parse } from "url";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Socket, createServer } from "net";
 import { ILocalContext, IRemoteContext, Registry } from "./index";
-
-const rl = createInterface({ input: stdin, output: stdout });
-
-const laddr = env.LADDR || "tcp://127.0.0.1:1337";
 
 let clients = 0;
 let counter = 0;
@@ -30,15 +27,109 @@ const registry = new Registry(
   {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     Println: async (ctx: IRemoteContext, msg: string) => {},
+  },
+  {
+    onClientConnect: () => {
+      clients++;
+
+      console.log(clients, "clients connected");
+    },
+    onClientDisconnect: () => {
+      clients--;
+
+      console.log(clients, "clients connected");
+    },
   }
 );
 
-const server = createServer(async (socket) => {
-  socket.on("error", (e) => {
-    console.error("Client disconnected with error:", e.cause);
+(async () => {
+  console.log(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
+
+- a: Print "Hello, world!
+`);
+
+  const rl = createInterface({ input: stdin, output: stdout });
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const line =
+      // eslint-disable-next-line no-await-in-loop
+      await rl.question("");
+
+    // eslint-disable-next-line no-await-in-loop
+    await registry.forRemotes(async (remoteID, remote) => {
+      console.log("Calling functions for remote with ID", remoteID);
+
+      switch (line) {
+        case "a":
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            await remote.Println(undefined, "Hello, world!");
+          } catch (e) {
+            console.error(`Got error for Increment func: ${e}`);
+          }
+          break;
+
+        default:
+          console.log(`Unknown letter ${line}, ignoring input`);
+      }
+    });
+  }
+})();
+
+const addr = env.ADDR || "127.0.0.1:1337";
+const listen = env.LISTEN !== "false";
+
+if (listen) {
+  const u = parse(`tcp://${addr}`);
+
+  const server = createServer(async (socket) => {
+    socket.on("error", (e) => {
+      console.error("Client disconnected with error:", e);
+    });
+
+    registry.linkTCPSocket(
+      socket,
+
+      JSON.stringify,
+      JSON.parse,
+
+      (v) => v,
+      (v) => v
+    );
   });
 
-  const remote = registry.linkTCPSocket(
+  server.listen(
+    {
+      host: u.hostname as string,
+      port: parseInt(u.port as string, 10),
+    },
+    () => console.log("Listening on", addr)
+  );
+} else {
+  const u = parse(`tcp://${addr}`);
+
+  const socket = new Socket();
+
+  socket.on("error", (e) => {
+    console.error("Disconnected with error:", e.cause);
+
+    exit(1);
+  });
+  socket.on("close", () => exit(0));
+
+  await new Promise<void>((res, rej) => {
+    socket.connect(
+      {
+        host: u.hostname as string,
+        port: parseInt(u.port as string, 10),
+      },
+      res
+    );
+    socket.on("error", rej);
+  });
+
+  registry.linkTCPSocket(
     socket,
 
     JSON.stringify,
@@ -47,47 +138,6 @@ const server = createServer(async (socket) => {
     (v) => v,
     (v) => v
   );
-  socket.on("close", () => {
-    clients--;
 
-    console.log(clients, "clients connected");
-  });
-
-  clients++;
-
-  console.log(clients, "clients connected");
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const line =
-      // eslint-disable-next-line no-await-in-loop
-      await rl.question(`Enter one of the following letters followed by <ENTER> to run a function on the remote(s):
-
-- a: Print "Hello, world!
-`);
-
-    switch (line) {
-      case "a":
-        try {
-          // eslint-disable-next-line no-await-in-loop
-          await remote.Println(undefined, "Hello, world!");
-        } catch (e) {
-          console.error(`Got error for Println func: ${e}`);
-        }
-        break;
-
-      default:
-        console.log(`Unknown letter ${line}, ignoring input`);
-    }
-  }
-});
-
-const u = parse(laddr);
-
-server.listen(
-  {
-    host: u.hostname as string,
-    port: parseInt(u.port as string, 10),
-  },
-  () => console.log("Listening on", laddr)
-);
+  console.log("Connected to", addr);
+}
